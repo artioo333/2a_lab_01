af4ce2a0e9ebec38772b08bc67131f29e76f06fc
Artur <artioo333000@gmail.com>
2019-11-08 10:37:51 +0100

    .

---
+++ Zadanie5/Zadanie5/Książka.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie5
+{
+    public class Książka
+    {
+        public string Tytuł;
+        public string Autor;
+        public int Regal;
+        public int Polka;
+        public int Miejsce;
+
+        public Książka(string tytuł, string autor, int regal, int polka, int miejsce)
+        {
+            Tytuł = tytuł;
+            Autor = autor;
+            Regal = regal;
+            Polka = polka;
+            Miejsce = miejsce;
+        }
+        public Książka()
+        {
+
+        }
+    }
+}
---
+++ Zadanie5/Zadanie5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Książka[,,] Magazyn = new Książka[3, 6, 10];
+            for(int i = 0; i < 3; i++) {
+                for (int n = 0; n < 6; n++) {
+                    for (int o = 0; o < 10; o++) {
+                        Magazyn[i,n,o] = new Książka("Potop", "Sienkiewicz", i,n,o);
+                    }
+                }
+            }
+            Magazyn[1,1,1] = new Książka()
+            {
+                Tytuł = "aaa",
+                Autor = "aa",
+                Regal = 1,
+                Polka = 1,
+                Miejsce = 1
+            };
+            Magazyn[2, 4, 8] = new Książka()
+            {
+                Tytuł = "Gra o Tron",
+                Autor = "RR.Martin",
+                Regal = 2,
+                Polka = 4,
+                Miejsce = 9
+            };
+            Console.WriteLine("Co potrzebne:");
+            string Książka = Console.ReadLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int n = 0; n < 6; n++)
+                {
+                    for (int o = 0; o < 10; o++)
+                    {
+                        if(Magazyn[i, n, o].Tytuł == Książka || Magazyn[i, n, o].Autor == Książka)
+                        {
+                            Console.WriteLine("Na regale:" + i + "\t Na półce:" + n + "\t Na miejscu:" +  o);
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
