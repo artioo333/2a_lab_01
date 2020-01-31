54495bbbaa22ee5ac7e5b4e94090234f81370f6f
Artur <artioo333000@gmail.com>
2020-01-31 09:21:04 +0100

    >

---
+++ Kolokwium_2/Kolokwium_2/Pomiar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium_2
+{
+    public class Pomiar
+    {
+        public double Temp { get; set; }
+        public double Wilgotnosc { get; set; }
+        public double PrWiatru { get; set; }
+        public double Cisnienie { get; set; }
+
+        public Pomiar(double temp, double wilgotnosc, double prWiatru, double cisnienie)
+        {
+            temp = Temp;
+            wilgotnosc = Wilgotnosc;
+            prWiatru = PrWiatru;
+            cisnienie = Cisnienie;
+        }
+    }
+}
